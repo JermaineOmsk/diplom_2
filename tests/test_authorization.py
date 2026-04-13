715f8cc1713592acc159dc0aeec70d6fdbdb42ba
JermaineOmsk <jermaine1989@yandex.ru>
2026-04-13 14:39:30 +0600

    добавлена allure аннотация, добавлена проверка статус кода

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -10,19 +10,25 @@ class TestAuthorization:
         payload = create_and_delete_user[1]
         login = payload.copy()
         login.pop("name")
-        response = requests.post(Endpoints.login_user, data=login)
-        assert response.json()["success"] is True
+        with allure.step("Отправка POST‑запроса на авторизацию пользователя"):
+            response = requests.post(Endpoints.login_user, data=login)
+        with allure.step("Проверка успешного ответа (success=True)"):
+            assert response.status_code == 200 and response.json()["success"] is True
 
     @allure.title("Проверка авторизации пользователя c неправильным логином и паролем код ответа")
     def test_authorization_user_code(self):
         payload = UserData.bad_login
-        response = requests.post(Endpoints.login_user, data=payload)
-        assert response.status_code == 401 
+        with allure.step("Отправка POST‑запроса с некорректными данными"):
+            response = requests.post(Endpoints.login_user, data=payload)
+        with allure.step("Проверка кода ответа 401"):
+            assert response.status_code == 401
 
     @allure.title("Проверка авторизации пользователя c неправильным логином и паролем текст ответа")
     def test_authorization_user_message(self):
         payload = UserData.bad_login
-        response = requests.post(Endpoints.login_user, data=payload)
-        assert response.json()['message'] == "email or password are incorrect"    
+        with allure.step("Отправка POST‑запроса с некорректными данными"):
+            response = requests.post(Endpoints.login_user, data=payload)
 
+        with allure.step("Проверка сообщения об ошибке в ответе"):
+            assert response.json()['message'] == "email or password are incorrect"
 
